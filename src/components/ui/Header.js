80faf053547390ccfa09aed9001f19b437038491
dejanV91 <dejanvukoicic91@gmail.com>
2023-01-08 19:42:18 +0100

    makeStyles for Header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,7 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import { useScrollTrigger } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import { makeStyles } from "@mui/styles";
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -17,14 +18,25 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+  },
+}));
+
 export default function Header(props) {
+  const classes = useStyles();
+
   return (
-    <ElevationScroll>
-      <AppBar position="fixed" color="primary">
-        <Toolbar>
-          <Typography variant="h3">Arc Development</Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <AppBar position="fixed" color="primary">
+          <Toolbar>
+            <Typography variant="h3">Arc Development</Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </>
   );
 }
