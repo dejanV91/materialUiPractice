57cbcdd3cf882e5c70ec32099a76b78208c192d3
dejanV91 <dejanvukoicic91@gmail.com>
2023-01-10 19:46:49 +0100

    WIP Tabs

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,8 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import { useScrollTrigger } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 import logo from "../../assets/logo.svg";
 
@@ -22,6 +24,7 @@ function ElevationScroll(props) {
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
+    marginBottom: "3em",
   },
   logo: {
     height: "7em",
@@ -36,6 +39,13 @@ export default function Header(props) {
         <AppBar position="fixed">
           <Toolbar disableGutters>
             <img className={classes.logo} src={logo} alt="company logo" />
+            <Tabs>
+              <Tab label="Home"></Tab>
+              <Tab label="Services"></Tab>
+              <Tab label="The Revolution"></Tab>
+              <Tab label="About Us"></Tab>
+              <Tab label="Contact Us"></Tab>
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
