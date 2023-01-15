ad3aba565ce67325fdce3dc6be7fe9d5d468b1c6
dejanV91 <dejanvukoicic91@gmail.com>
2023-01-15 11:38:19 +0100

    Finish Links

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -56,6 +56,22 @@ export default function Header() {
   const { classes } = useStyles();
   const [value, setValue] = useState(0);
 
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/aboutus" && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === "/contactus" && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
+
   const handleTabs = (e, value) => {
     setValue(value);
   };
