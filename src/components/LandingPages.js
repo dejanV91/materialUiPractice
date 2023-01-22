94ee3624ce5a71481525f57c93ece79fe1769ac4
dejanV91 <dejanvukoicic91@gmail.com>
2023-01-22 15:45:05 +0100

    hero block

--- src/components/LandingPages.js
+++ src/components/LandingPages.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Lottie from "react-lottie";
 import { makeStyles } from "tss-react/mui";
+import Grid from "@mui/material/Grid";
+import { Button } from "@mui/material";
 
 import animationData from "../animations/landinganimation/data";
 
@@ -18,5 +20,29 @@ export default function LandingPage() {
     },
   };
 
-  return <Lottie options={defaultOptions} height={"100%"} width={"100%"} />;
+  return (
+    <React.Fragment>
+      <Grid container direction={"column"}>
+        <Grid item>
+          <Grid container direction="row">
+            <Grid item>
+              <div>
+                Bringing West Coast Technology <br />
+                to the Midwest
+              </div>
+              <Grid container>
+                <Grid item>
+                  <Button variant="contained">Free Estimate</Button>
+                </Grid>
+              </Grid>
+              {/* <Lottie options={defaultOptions} height={"100%"} width={"100%"} /> */}
+            </Grid>
+            <Grid item>
+              <div>Dejan Vukoicic</div>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 }
