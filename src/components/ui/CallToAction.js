ab198f5733c301a4844af7efb069c434104941e3
dejanV91 <dejanvukoicic91@gmail.com>
2023-01-26 21:54:00 +0100

    Setup CallToAction file

---
+++ src/components/ui/CallToAction.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { makeStyles } from "tss-react/mui";
+
+const useStyle = makeStyles()((theme) => {});
+
+export default function CallToAction() {
+  const classes = useStyle();
+
+  return <Grid container></Grid>;
+}
