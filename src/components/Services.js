a98ba1d9f9a89565d46b0fdaf7d2d61380ea65d1
dejanV91 <dejanvukoicic91@gmail.com>
2023-01-30 15:10:03 +0100

    Finish Services page setup

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,14 +6,39 @@ import { Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 
 import customSoftwareIcon from "../assets/Custom Software Icon.svg";
 import mobileAppsIcon from "../assets/mobileIcon.svg";
 import websitesIcon from "../assets/websiteIcon.svg";
 
-const useStyles = makeStyles()((theme) => ({}));
+const useStyles = makeStyles()((theme) => ({
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: "0.7rem",
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down("md")]: { marginBottom: "2em" },
+  },
+  specialText: {
+    fontFamily: "Pacifico",
+    color: theme.palette.common.orange,
+  },
+  subtitle: {
+    marginBottom: "1em",
+  },
+  icon: {
+    marginLeft: "2em",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+    },
+  },
+  serviceContainer: {
+    marginTop: "12em",
+    [theme.breakpoints.down("md")]: {
+      padding: 25,
+    },
+  },
+}));
 
 export default function Services(props) {
   const { classes } = useStyles();
@@ -22,51 +47,6 @@ export default function Services(props) {
 
   return (
     <Grid container direction="column">
-      <Grid item>
-        {/*----Custom Software   Block ----*/}
-        <Grid
-          container
-          justifyContent={matchesSM ? "center" : undefined}
-          direction="row"
-          className={classes.serviceContainer}
-        >
-          <Grid
-            item
-            style={{
-              marginLeft: matchesSM ? 0 : "5em",
-              textAlign: matchesSM ? "center" : undefined,
-            }}
-          >
-            <Typography variant="h4">Custom Software Development</Typography>
-            <Typography variant="subtitle1" className={classes.subtitle}>
-              Save Energy. Save Time. Save Monay
-            </Typography>
-            <Typography variant="subtitle1">
-              Complete digital solutions, from investigation to{" "}
-              <span className={classes.specialText}>celebration</span>
-            </Typography>
-            <Button
-              component={Link}
-              to="/customs"
-              variant="outlined"
-              className={classes.learnButton}
-              onClick={() => {
-                props.setValue(1);
-                props.setSelectedIndex(1);
-              }}
-            >
-              Learn More
-            </Button>
-          </Grid>
-          <Grid item>
-            <img
-              className={classes.icon}
-              src={customSoftwareIcon}
-              alt="Custom Software icon"
-            />
-          </Grid>
-        </Grid>
-      </Grid>
       <Grid item>
         {/*----Mobile Block ----*/}
         <Grid
@@ -112,7 +92,7 @@ export default function Services(props) {
         </Grid>
       </Grid>
       <Grid item>
-        {/*----Websites Block ----*/}
+        {/*----Custom Software   Block ----*/}
         <Grid
           container
           justifyContent={matchesSM ? "center" : undefined}
@@ -123,6 +103,51 @@ export default function Services(props) {
             item
             style={{
               marginLeft: matchesSM ? 0 : "5em",
+              textAlign: matchesSM ? "center" : undefined,
+            }}
+          >
+            <Typography variant="h4">Custom Software Development</Typography>
+            <Typography variant="subtitle1" className={classes.subtitle}>
+              Save Energy. Save Time. Save Monay
+            </Typography>
+            <Typography variant="subtitle1">
+              Complete digital solutions, from investigation to{" "}
+              <span className={classes.specialText}>celebration</span>
+            </Typography>
+            <Button
+              component={Link}
+              to="/customs"
+              variant="outlined"
+              className={classes.learnButton}
+              onClick={() => {
+                props.setValue(1);
+                props.setSelectedIndex(1);
+              }}
+            >
+              Learn More
+            </Button>
+          </Grid>
+          <Grid item>
+            <img
+              className={classes.icon}
+              src={customSoftwareIcon}
+              alt="Custom Software icon"
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid item>
+        {/*----Websites Block ----*/}
+        <Grid
+          container
+          justifyContent={matchesSM ? "center" : "flex-end"}
+          direction="row"
+          className={classes.serviceContainer}
+        >
+          <Grid
+            item
+            style={{
               textAlign: matchesSM ? "center" : undefined,
             }}
           >
@@ -146,55 +171,13 @@ export default function Services(props) {
               Learn More
             </Button>
           </Grid>
-          <Grid item>
+          <Grid item style={{ marginRight: matchesSM ? 0 : "5em" }}>
             <img
               className={classes.icon}
               src={websitesIcon}
               alt="website icon"
             />
           </Grid>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Grid
-          container
-          style={{ height: "100em", marginTop: "12em" }}
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Card className={classes.revolutionCard}>
-            <CardContent>
-              <Grid
-                container
-                direction="column"
-                style={{ textAlign: "center" }}
-              >
-                <Grid item>
-                  <Typography variant="h3" gutterBottom>
-                    The Revolution
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Typography variant="subtitle1">
-                    Visionary insights coupled with cutting-edge technology is a
-                    recipe for revolution.
-                  </Typography>
-                  <Button
-                    component={Link}
-                    to="/revolution"
-                    className={classes.learnButtonHero}
-                    variant="outlined"
-                    onClick={() => {
-                      props.setValue(2);
-                    }}
-                  >
-                    Learn More
-                  </Button>
-                </Grid>
-              </Grid>
-            </CardContent>
-          </Card>
-          <div className={classes.revolutionBackground}></div>
         </Grid>
       </Grid>
     </Grid>
