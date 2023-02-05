b44f696b6758a362bc32184c9db4ec889f769bea
dejanV91 <dejanvukoicic91@gmail.com>
2023-02-05 10:20:08 +0100

    State Controlled Values

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,10 +6,12 @@ import { Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 import background from "../assets/background.jpg";
 import phoneIcon from "../assets/phone.svg";
 import emailIcon from "../assets/email.svg";
+import airplane from "../assets/send.svg";
 
 const useStyles = makeStyles()((theme) => ({
   background: {
@@ -19,15 +21,38 @@ const useStyles = makeStyles()((theme) => ({
     backgroundRepeat: "no-repeat",
     height: "60em",
   },
+  estimateButton: {
+    ...theme.typography.estimate,
+    borderRadius: 50,
+    height: 80,
+    width: 205,
+    backgroundColor: theme.palette.common.orange,
+    fontSize: "1rem",
+    marginRight: "5em",
+    marginLeft: "2em",
+    [theme.breakpoints.down("md")]: {
+      marginLeft: 0,
+      marginRight: 0,
+    },
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: "0.7rem",
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down("md")]: { marginBottom: "2em" },
+  },
 }));
 
-export default function Contact() {
+export default function Contact(props) {
   const { classes } = useStyles();
   const theme = useTheme();
+  const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
 
   return (
     <Grid container direction="row">
@@ -84,6 +109,7 @@ export default function Contact() {
               id="name"
               variant="standard"
               value={name}
+              onChange={(event) => setName(event.target.value)}
             ></TextField>
           </Grid>
           <Grid item>
@@ -92,6 +118,7 @@ export default function Contact() {
               id="emal"
               variant="standard"
               value={email}
+              onChange={(event) => setEmail(event.target.value)}
             ></TextField>
           </Grid>
           <Grid item>
@@ -100,11 +127,83 @@ export default function Contact() {
               id="phone"
               variant="standard"
               value={phone}
+              onChange={(event) => setPhone(event.target.value)}
             ></TextField>
           </Grid>
         </Grid>
+        <Grid item>
+          <TextField
+            variant="standard"
+            multiline
+            rows={10}
+            value={message}
+            id="message"
+            onChange={(event) => {
+              setMessage(event.target.value);
+            }}
+          ></TextField>
+        </Grid>
+        <Button variant="contained">
+          Send Message <img src={airplane} alt="paper airplane" />
+        </Button>
       </Grid>
-      <Grid item container className={classes.background} lg={9}></Grid>
+      <Grid
+        item
+        container
+        className={classes.background}
+        alignItems="center"
+        lg={9}
+      >
+        <Grid
+          item
+          style={{
+            marginLeft: matchesMD ? 0 : "5em",
+            textAlign: matchesMD ? "center" : "inherit",
+          }}
+        >
+          <Grid container direction="column">
+            <Grid item>
+              <Typography variant="h2">
+                Simple Software.
+                <br /> Revolutionary Results.
+              </Typography>
+              <Typography variant="subtitle2" style={{ fontSize: "1.5rem" }}>
+                Take advatage of the 21th Century.
+              </Typography>
+              <Grid
+                container
+                justifyContent={matchesMD ? "center" : undefined}
+                item
+              >
+                <Button
+                  component={Link}
+                  to="/revolution"
+                  variant="outlined"
+                  className={classes.learnButton}
+                  onClick={() => {
+                    props.setValue(2);
+                  }}
+                >
+                  Learn More
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Button
+            component={Link}
+            to="/estimate"
+            variant="contained"
+            className={classes.estimateButton}
+            onClick={() => {
+              props.setValue(5);
+            }}
+          >
+            Free Estimate
+          </Button>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
