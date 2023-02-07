ccf0dd3b7633fc0907ab01938304b2fee3666b99
dejanV91 <dejanvukoicic91@gmail.com>
2023-02-07 19:22:36 +0100

    WIP Using Nodemailer

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,10 @@
 const functions = require("firebase-functions");
-const config = functions.config();
 const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+const config = functions.config();
 admin.initializeApp();
+
+const transporter = nodemailer.createTransport({});
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
