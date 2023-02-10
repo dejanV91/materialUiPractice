f08da160f7cc79be8b44e3342d574c2ce731259e
dejanV91 <dejanvukoicic91@gmail.com>
2023-02-10 20:28:31 +0100

    WIP Firebase fix

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require("firebase-functions");
 
+const config = functions.config();
+
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
