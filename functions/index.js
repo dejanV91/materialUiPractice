405c06f449c4b5840464463b099457a719cd421c
dejanV91 <dejanvukoicic91@gmail.com>
2023-02-11 19:52:08 +0100

    WIP Firebase fix

--- functions/index.js
+++ functions/index.js
@@ -3,14 +3,24 @@ const admin = require("firebase-admin");
 const nodemailer = require("nodemailer");
 const config = functions.config();
 
-const transporet = nodemailer.createTransport({ service: "Gmail", auth: {} });
+const transporet = nodemailer.createTransport({
+  service: "Gmail",
+  auth: { user: config.user.email, pass: config.user.password },
+});
+
+let mailOptions = {
+  from: "Material - UI",
+  to: "dejanvukoicic91@gmail.com",
+  subject: "Testing nodemailer",
+  text: "Test successful",
+};
 
 admin.initializeApp();
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+exports.sendMail = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
