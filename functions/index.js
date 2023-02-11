7399d5593e4cf383198d637cd9feffbbb7949218
dejanV91 <dejanvukoicic91@gmail.com>
2023-02-11 19:31:01 +0100

    WIP Firebase fix

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,11 @@
 const functions = require("firebase-functions");
-
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
 const config = functions.config();
+
+const transporet = nodemailer.createTransport({ service: "Gmail", auth: {} });
+
+admin.initializeApp();
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
